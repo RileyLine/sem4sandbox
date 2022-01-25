cf0df5f51d505589705b95bf796f9faaf5079969
Riley Line <line.riley@gmail.com>
2022-01-25 13:44:30 -0500

    changed menu to 'menu-primary'

--- themes/own/functions.php
+++ themes/own/functions.php
@@ -49,7 +49,7 @@ function own_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'own' ),
+			'menu-primary' => esc_html__( 'Primary', 'own' ),
 		)
 	);
 
